95cb006c6ea43cc09c47d68fe9c7dc4597a3ed3e
Andrew Gwozdziewycz <apg@heroku.com>
2016-05-25 22:16:46 -0700

    Update info

--- info.rkt
+++ info.rkt
@@ -4,6 +4,6 @@
                "rackunit-lib"))
 (define build-deps '("scribble-lib" "racket-doc"))
 (define scribblings '(("scribblings/cfdg.scrbl" ())))
-(define pkg-desc "CFDG")
+(define pkg-desc "CFDG: Simple implementation of Chris Coyne's original Context Free Data Grammars")
 (define version "0.0")
 (define pkg-authors '(apg))
